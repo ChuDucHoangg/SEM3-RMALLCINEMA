f27bfaae69abe527066df87427cc2e6ffa8e172e
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-12-16 23:35:48 +0700

    cut blog page and fix js

---
+++ src/components/views/blog/search.js
@@ -0,0 +1,180 @@
+function Search_Blog() {
+    return (
+        <>
+            <div className="col-lg-4 col-sm-10 col-md-8">
+                <aside>
+                    <div className="widget widget-search">
+                        <h5 className="title">search</h5>
+                        <form className="search-form">
+                            <input type="text" placeholder="Search Here..." required />
+                            <button type="submit">
+                                <i className="flaticon-loupe"></i>Search
+                            </button>
+                        </form>
+                    </div>
+                    <div className="widget widget-post">
+                        <h5 className="title">latest post</h5>
+                        <div className="slider-nav">
+                            <span className="far fa-angle-left widget-prev"></span>
+                            <span className="far fa-angle-right widget-next"></span>
+                        </div>
+                        <div className="widget-slider owl-carousel owl-theme">
+                            <div className="item">
+                                <div className="thumb">
+                                    <a href="#">
+                                        <img src="assets/img/blog/slider.jpg" alt="blog" />
+                                    </a>
+                                </div>
+                                <div className="content">
+                                    <h6 className="p-title">
+                                        <a href="#">There are many variations of passages</a>
+                                    </h6>
+                                    <div className="meta-post">
+                                        <a href="#" className="mr-4">
+                                            <i className="fal fa-clock fa-lg"></i>Feb 12, 2023
+                                        </a>
+                                        <a href="#">
+                                            <i className="fal fa-comments-alt"></i>40 Comments
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="thumb">
+                                    <a href="#">
+                                        <img src="assets/img/blog/slider.jpg" alt="blog" />
+                                    </a>
+                                </div>
+                                <div className="content">
+                                    <h6 className="p-title">
+                                        <a href="#">There are many variations of passages</a>
+                                    </h6>
+                                    <div className="meta-post">
+                                        <a href="#" className="mr-4">
+                                            <i className="fal fa-clock"></i>Feb 12, 2023
+                                        </a>
+                                        <a href="#">
+                                            <i className="fal fa-comments-alt"></i>40 Comments
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="widget widget-categories">
+                        <h5 className="title">categories</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Latest Movie
+                                    </span>
+                                    <span>20</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Latest Trailers
+                                    </span>
+                                    <span>50</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Upcoming Events
+                                    </span>
+                                    <span>40</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Coming Soon
+                                    </span>
+                                    <span>25</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> National League
+                                    </span>
+                                    <span>15</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Latest Events
+                                    </span>
+                                    <span>30</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>
+                                        <i className="fal fa-caret-right"></i> Internation Sports{" "}
+                                    </span>
+                                    <span>25</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="widget widget-follow">
+                        <h5 className="title">Follow Us</h5>
+                        <ul className="social-icons">
+                            <li>
+                                <a href="#" className>
+                                    <i className="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="active">
+                                    <i className="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className>
+                                    <i className="fab fa-pinterest-p"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i className="fab fa-google"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i className="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="widget widget-tags">
+                        <h5 className="title">featured tags</h5>
+                        <ul>
+                            <li>
+                                <a href="#">digital</a>
+                            </li>
+                            <li>
+                                <a href="#">sports</a>
+                            </li>
+                            <li>
+                                <a href="#">events</a>
+                            </li>
+                            <li>
+                                <a href="#">movie</a>
+                            </li>
+                            <li>
+                                <a href="#">modern</a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </>
+    );
+}
+export default Search_Blog;
--- src/components/views/home/blog.js
+++ src/components/views/home/blog.js
@@ -1,74 +1,74 @@
 function Blog() {
     return (
         <>
-            <section class="movie-section padding-bottom bg-two">
-                <div class="container">
-                    <div class="row flex-wrap-reverse justify-content-center">
-                        <div class="col-lg-12">
-                            <div class="article-section">
-                                <div class="section-header-1">
-                                    <h2 class="title">Blog</h2>
-                                    <a class="view-more" href="blog.html">
-                                        View More <i class="fal fa-long-arrow-alt-right"></i>{" "}
+            <section className="movie-section padding-bottom bg-two">
+                <div className="container">
+                    <div className="row flex-wrap-reverse justify-content-center">
+                        <div className="col-lg-12">
+                            <div className="article-section">
+                                <div className="section-header-1">
+                                    <h2 className="title">Blog</h2>
+                                    <a className="view-more" href="blog.html">
+                                        View More <i className="fal fa-long-arrow-alt-right"></i>{" "}
                                     </a>
                                 </div>
-                                <div class="row mb-30-none justify-content-center">
-                                    <div class="col-sm-6 col-lg-4">
-                                        <div class="blog-grid">
-                                            <div class="movie-thumb c-thumb">
+                                <div className="row mb-30-none justify-content-center">
+                                    <div className="col-sm-6 col-lg-4">
+                                        <div className="blog-grid">
+                                            <div className="movie-thumb c-thumb">
                                                 <a href="#">
                                                     <img src="assets/img/blog/blog-1.jpg" alt="blog" />
                                                 </a>
-                                                <div class="event-date">
-                                                    <h6 class="date-title">11</h6>
+                                                <div className="event-date">
+                                                    <h6 className="date-title">11</h6>
                                                     <span>Feb</span>
                                                 </div>
                                             </div>
-                                            <div class="blog-content">
-                                                <h5 class="title">There are many variations of passages</h5>
+                                            <div className="blog-content">
+                                                <h5 className="title">There are many variations of passages</h5>
                                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
                                                 <a href>
-                                                    Read More <i class="fal fa-long-arrow-alt-right"></i>
+                                                    Read More <i className="fal fa-long-arrow-alt-right"></i>
                                                 </a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-sm-6 col-lg-4">
-                                        <div class="blog-grid">
-                                            <div class="movie-thumb c-thumb">
+                                    <div className="col-sm-6 col-lg-4">
+                                        <div className="blog-grid">
+                                            <div className="movie-thumb c-thumb">
                                                 <a href="#">
                                                     <img src="assets/img/blog/blog-2.jpg" alt="blog" />
                                                 </a>
-                                                <div class="event-date">
-                                                    <h6 class="date-title">11</h6>
+                                                <div className="event-date">
+                                                    <h6 className="date-title">11</h6>
                                                     <span>Feb</span>
                                                 </div>
                                             </div>
-                                            <div class="blog-content">
-                                                <h5 class="title">Contrary to popular belief distracted</h5>
+                                            <div className="blog-content">
+                                                <h5 className="title">Contrary to popular belief distracted</h5>
                                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
                                                 <a href>
-                                                    Read More <i class="fal fa-long-arrow-alt-right"></i>
+                                                    Read More <i className="fal fa-long-arrow-alt-right"></i>
                                                 </a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-sm-6 col-lg-4">
-                                        <div class="blog-grid">
-                                            <div class="movie-thumb c-thumb">
+                                    <div className="col-sm-6 col-lg-4">
+                                        <div className="blog-grid">
+                                            <div className="movie-thumb c-thumb">
                                                 <a href="#">
                                                     <img src="assets/img/blog/blog-3.jpg" alt="blog" />
                                                 </a>
-                                                <div class="event-date">
-                                                    <h6 class="date-title">11</h6>
+                                                <div className="event-date">
+                                                    <h6 className="date-title">11</h6>
                                                     <span>Feb</span>
                                                 </div>
                                             </div>
-                                            <div class="blog-content">
-                                                <h5 class="title">Sed ut perspiciatis unde omnis iste</h5>
+                                            <div className="blog-content">
+                                                <h5 className="title">Sed ut perspiciatis unde omnis iste</h5>
                                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
                                                 <a href>
-                                                    Read More <i class="fal fa-long-arrow-alt-right"></i>
+                                                    Read More <i className="fal fa-long-arrow-alt-right"></i>
                                                 </a>
                                             </div>
                                         </div>
