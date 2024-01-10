4311ffdff65e09e3ac849c1311b9ed18cbb7b591
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-01-10 17:47:14 +0700

    Update Home & Footer

--- src/components/views/home/movies.js
+++ src/components/views/home/movies.js
@@ -4,15 +4,19 @@ import url from "../../../services/url";
 import { Link } from "react-router-dom";
 
 function Movies() {
-    const [movies, setMovies] = useState([]);
+    const [comingSoon, setComingSoon] = useState([]);
+    const [lastShowing, setLastShowing] = useState([]);
+    const [bestShowing, setBestShowing] = useState([]);
 
     const loadMovie = useCallback(async () => {
         try {
-            const moviesResponse = await api.get(url.MOVIE.LIST);
+            const comingSoonResponse = await api.get(url.MOVIE.COMING_SOON);
+            const lastShowingResponse = await api.get(url.MOVIE.LATEST_SHOWING);
+            const bestShowingResponse = await api.get(url.MOVIE.BEST_SHOWING);
 
-            const limitedMovies = moviesResponse.data.slice(0, 6);
-
-            setMovies(limitedMovies);
+            setComingSoon(comingSoonResponse.data);
+            setLastShowing(lastShowingResponse.data);
+            setBestShowing(bestShowingResponse.data);
         } catch (error) {
             console.log(error);
         }
@@ -30,14 +34,114 @@ function Movies() {
                         <div className="col-lg-12">
                             <div className="article-section padding-bottom">
                                 <div className="section-header-1">
-                                    <h2 className="title">movies</h2>
+                                    <h2 className="title">Coming Soon</h2>
 
                                     <Link to="/movies" className="view-more">
                                         View More <i className="fal fa-long-arrow-alt-right"></i>
                                     </Link>
                                 </div>
                                 <div className="row mb-30-none justify-content-center">
-                                    {movies.map((item, index) => {
+                                    {comingSoon.map((item, index) => {
+                                        return (
+                                            <div className="col-sm-6 col-lg-4" key={index}>
+                                                <div className="movie-grid">
+                                                    <div className="movie-thumb c-thumb h-200">
+                                                        <Link to={`/movie-details/${item.id}`}>
+                                                            <img src={item.movie_image} alt="movie" />
+                                                        </Link>
+                                                    </div>
+                                                    <div className="movie-content">
+                                                        <h5 className="title m-0">
+                                                            <Link to={`/movie-details/${item.id}`} className="line-clamp">
+                                                                {item.title}
+                                                            </Link>
+                                                        </h5>
+                                                        <ul className="movie-rating-percent">
+                                                            <li>
+                                                                <i className="fal fa-clock"></i>
+                                                                <span className="content">{item.duration} minutes</span>
+                                                            </li>
+                                                            <li>
+                                                                <i className="fal fa-heart"></i>
+                                                                <span className="content">{item.favoriteCount}</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="movie-section padding-top bg-two">
+                <div className="container">
+                    <div className="row flex-wrap-reverse justify-content-center">
+                        <div className="col-lg-12">
+                            <div className="article-section padding-bottom">
+                                <div className="section-header-1">
+                                    <h2 className="title">Last Showing</h2>
+
+                                    <Link to="/movies" className="view-more">
+                                        View More <i className="fal fa-long-arrow-alt-right"></i>
+                                    </Link>
+                                </div>
+                                <div className="row mb-30-none justify-content-center">
+                                    {lastShowing.map((item, index) => {
+                                        return (
+                                            <div className="col-sm-6 col-lg-4" key={index}>
+                                                <div className="movie-grid">
+                                                    <div className="movie-thumb c-thumb h-200">
+                                                        <Link to={`/movie-details/${item.id}`}>
+                                                            <img src={item.movie_image} alt="movie" />
+                                                        </Link>
+                                                    </div>
+                                                    <div className="movie-content">
+                                                        <h5 className="title m-0">
+                                                            <Link to={`/movie-details/${item.id}`} className="line-clamp">
+                                                                {item.title}
+                                                            </Link>
+                                                        </h5>
+                                                        <ul className="movie-rating-percent">
+                                                            <li>
+                                                                <i className="fal fa-clock"></i>
+                                                                <span className="content">{item.duration} minutes</span>
+                                                            </li>
+                                                            <li>
+                                                                <i className="fal fa-heart"></i>
+                                                                <span className="content">{item.favoriteCount}</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="movie-section padding-top bg-two">
+                <div className="container">
+                    <div className="row flex-wrap-reverse justify-content-center">
+                        <div className="col-lg-12">
+                            <div className="article-section padding-bottom">
+                                <div className="section-header-1">
+                                    <h2 className="title">Best Showing</h2>
+
+                                    <Link to="/movies" className="view-more">
+                                        View More <i className="fal fa-long-arrow-alt-right"></i>
+                                    </Link>
+                                </div>
+                                <div className="row mb-30-none justify-content-center">
+                                    {bestShowing.map((item, index) => {
                                         return (
                                             <div className="col-sm-6 col-lg-4" key={index}>
                                                 <div className="movie-grid">
